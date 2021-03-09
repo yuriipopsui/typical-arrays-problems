c8800d4d67d9b54b940b00c098cda2599ccb74a8
Yurii Popsui <yp.onward@gmail.com>
2021-03-09 14:25:45 +0200

    Typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+
+exports.min = function(array){
+  if(arguments.length>0){
+    return array.length===0 ? 0 : Math.min.apply(null, array); 
+  }
+ return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length>0){
+    return array.length===0 ? 0 : Math.max.apply(null, array); 
+  }
+ return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length>0){
+    return array.length===0 ? 0 : array.reduce((acc, cur) => (acc + cur), 0) / array.length;
+  }
+ return 0;
 }
